7a02e1011b7607887189125397a610ae2a477775
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-23 02:02:04 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,12 @@
 class Backer
   
-  attr_reader :name
+  attr_reader :name, :backed_projects
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-  
-  def backed_projects
-    @backed_projects
-  end
-  
+
   def back_project(project)
     Project.backers.collect {
     
