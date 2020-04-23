b6265feb611a056b7a6db5b6b8e9bee3b0a6fc7c
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-23 02:04:12 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,11 @@
 class Project
   
-  attr_reader :title
+  attr_reader :title, :backers
   
   def initialize(title)
     @title = title
     @backers = []
   end
-  
-  def backers
-    @backers
-  end
+
   
 end
